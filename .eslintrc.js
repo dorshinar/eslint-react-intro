72eba56ac9691a0316bb66fb96e349d7f08538e5
dorshinar <dors718@gmail.com>
2019-01-21 21:35:16 +0200

    Add files

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'react-app',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {},
+};
